00cb9fcf5625c222bf8b4da3b898694270551f02
venegrad <venegrad@gmail.com>
2019-06-04 11:17:10 +0300

    Add api and json settings support

--- gulpfile.js
+++ gulpfile.js
@@ -13,11 +13,30 @@ const concat = require('gulp-concat')
 const cssnano = require('gulp-cssnano')
 const uglify = require('gulp-uglifyjs')
 const imagemin = require('gulp-imagemin')
-
+const tinypng = require('gulp-tinypng')
+const log = require('fancy-log')
 const isDev = process.env.NODE_ENV === 'development'
 
 const srcPath = './src/'
 const dstPath = './dist/'
+
+const jsonSettings = 'C:/Users/Venegrad/OneDrive/Work-cloud/settings.json';
+let checkJson = 1;
+let jsonSet = 0;
+
+
+try {
+  let fileJson = require(jsonSettings);
+} catch (error) {
+  console.log('file with ftp setting is empty or not exist');
+  checkJson = 0;
+}
+
+if(checkJson == 1) {
+  jsonSet = require(jsonSettings);
+}
+
+
 
 if ( isDev ) {
   gulp.task('serve',  function() {
@@ -44,11 +63,33 @@ if ( isDev ) {
   gulp.task('reload', function(done) { browserSync.reload(); done() })
 }
 
+
+const imageminOptions = {
+  verbose: true,
+  interlaced: true,
+  progressive: true,
+  optimizationLevel: 5,
+  plugins: [
+    imagemin.gifsicle({interlaced: true}),
+    imagemin.jpegtran({progressive: true}),
+    imagemin.optipng({optimizationLevel: 5}),
+    imagemin.svgo({plugins: [{removeViewBox: true},{cleanupIDs: false}]})
+  ]
+}
+
 gulp.task('build:img', function() {
   return gulp.src(srcPath+'img/**/*')
-    .pipe( isDev ? noop() : imagemin() )
+    .pipe( isDev ? noop() : imagemin(imageminOptions))
     .pipe( gulp.dest(dstPath+'img'))
 })
+
+
+
+gulp.task('tinypng', function () {
+  return gulp.src(srcPath+'img/**/*')
+      .pipe(tinypng(jsonSet.tinypngkey))
+      .pipe(gulp.dest(srcPath+'img'));
+});
 
 gulp.task('build:php', function() {
   return gulp.src(srcPath+'php/**/*')
@@ -119,6 +160,8 @@ gulp.task('clean', function(){
 })
 
 gulp.task('build', gulp.series('build:js', 'build:img', 'build:php', 'build:static', 'build:icons', 'build:css', 'build:html', 'build:fonts') )
+gulp.task('build:final', gulp.series('tinypng', 'build'))
+
 
 // start
 defaultTask = ['clean', 'build']
--- install.bat
+++
@@ -1,3 +0,0 @@
-npm i gulp -g && npm i
-
-
